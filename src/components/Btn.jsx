b0a4345f17706ba2adbdf3728b647669af6fd838
sidhantabasa <112327515+sidhantabasa@users.noreply.github.com>
2024-08-17 22:20:13 +0530

    Update Btn.jsx

--- src/components/Btn.jsx
+++ src/components/Btn.jsx
@@ -4,7 +4,7 @@ const Btn = ({name}) => {
   return (
     <div>
     <button className='py-1 px-6 my-2 mx-1 bg-gray-200 rounded-lg hover:bg-gray-400'>{name}</button>
-    </div>
+    </div> 
   )
 }
 
